1065263c1d41de5b27a3f2d232e5e9970b79db0e
Isaac Thill <ithill22@turing.edu>
2023-01-04 14:05:01 -0700

    Initial commit

---
+++ array.rb
@@ -0,0 +1,4 @@
+students = ["Isaac", "Julian", "Alejandro", "Jermaine"]
+class_size = [30, 28, 17, 21]
+tuition_owed = [1200.67, 300.12, 700.25, 2518.17]
+greater_than_1000 = [true, false, false, true]
