236a773524819ce2d4989b052f5caec40360508b
Isaac Thill <ithill22@turing.edu>
2023-01-04 17:43:51 -0700

    Fix issue with array.assoc

--- array.rb
+++ array.rb
@@ -20,18 +20,24 @@ p greater_than_1000
 # Index positions refer to the order of objects in an array. 
 # Index positions start with index position 0 and move up in increments of 1
 # Consider the students array
-Students[0]
+students[0]
 # this refers to Isaac in the array above as he is in index position 0
-Students[3]
+students[3]
 # this refers to Jermaine as he is in position 3.
 
 # Demonstrate Array Method of Choice
-# .assoc will search through a series of indicated arrays and calls the first
-# associated with the given object
+# .assoc is used to search through an array of arrays whose attached object
+# is compared with the index contained in the contained arrays. If a match is found
+# it will output the associated array, otherwise it will output nil.
+
+students = ["Isaac", "Julian", "Alejandro", "Jermaine"]
+class_size = [30, 28, 17, 21]
+tuition_owed = [1200.67, 300.12, 700.25, 2518.17]
+greater_than_1000 = [true, false, false, true]
 
 arrays = [students, class_size, tuition_owed, greater_than_1000]
 
-p arrays.assoc(Alejandro)
+p arrays.assoc(1200.67)
 
-# This should output the array: Students
+# This should output the array: tuition_owed
 
